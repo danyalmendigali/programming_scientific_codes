2c16ecc6f9fea32efa80218de07249f789039c1d
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-10 00:24:33 +0600

    dg

---
+++ list_scientific_codes/list_push_front().cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve() {
+     vector <int> numbers_vector {1, 2, 3, 4, 5};
+     list <int> numbers_list {1, 2, 3, 4, 5};
+}
+
+int main() {
+     cin.tie(0);
+
+     int t;
+     cin >> t;
+     while(t--)
+        solve();
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
