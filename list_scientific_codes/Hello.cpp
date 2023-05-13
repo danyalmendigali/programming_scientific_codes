ffebff7639e8b816bc25f72a50610efa3a65d5fa
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-13 14:35:46 +0600

    list

---
+++ list_scientific_codes/Hello.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    cout << "Привет"; ///  как дела
+
+    return 0;
+
+}
