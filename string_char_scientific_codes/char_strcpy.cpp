79f8862330438a08ecbe94c802be9e8cc8ae22c2
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-29 23:45:25 +0600

    string_char

---
+++ string_char_scientific_codes/char_strcpy.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Программа заменяет числа в строке c s1 до s2 своими числами
+
+void solve() {
+    char sentences[200];
+    cout << "Введите строку: ";
+    cin.getline(sentences, 200); // Чтение предложения с пробелами
+    cout << "Введите индекс с которого хотите изменить строку: ";
+    int s1, s2;
+    cin >> s1;
+    cout << "Введите индекс по какой хотите изменить строку: ";
+    cin >> s2;
+
+    cin.ignore();
+    cout << "Введите " << s2 - s1 + 1 << " элементов" << endl;
+
+    for(int i = s1; i <= s2; i++) {
+        char a;
+        cout << "Элемент " << i << " = ";
+        cin >> a;
+        sentences[i] = a;
+    }
+
+    for(int i = 0; i < strlen(sentences); i++) {
+        if(sentences[i] == ' ') {
+            break;
+        }
+
+        cout << sentences[i];
+    }
+
+
+
+
+}
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+    solve();
+
+
+
+
+    return 0;
+}
