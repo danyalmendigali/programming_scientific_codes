e08708c365288f70f1f0347a5e5ec0ac10bfc162
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-04 17:46:49 +0600

    functions

---
+++ functions/ref.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    void intfrac(float, float&, float&);
+    float number, intpart, fracpart;
+    do {
+        cout << "Введите вещественное число: ";
+        cin >> number; // ввод числа пользователем
+        intfrac(number, intpart, fracpart); // нахождение целой и дробной части
+        cout << "Целая часть равна " << intpart << ", дробная часть равна " << fracpart << endl;
+    }
+    while(number != 0.0); // выход из цикла, если введен ноль
+
+
+
+  return 0;
+}
+
+void intfrac(float n, float& intp, float& fracp)
+{
+    long temp = static_cast<long>(n); // преобразование к типу long
+    intp = static_cast<float>(temp); // и обратно во float
+    fracp = n - intp; // вычитаем целую часть
+}
