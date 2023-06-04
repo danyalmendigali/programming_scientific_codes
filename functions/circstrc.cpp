3554a631ecdb83e32f79ee67f8527dd50482849e
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-04 14:38:23 +0600

    functions

---
+++ functions/circstrc.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <borlacon.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+struct Distance
+{
+    int feet;
+    int inches;
+};
+
+void engldisp(Distance);
+
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+  return 0;
+}
