7fbad03f24a7bad3301aeaa3e74d5fc34976f881
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-04 11:26:01 +0600

    functions

---
+++ functions/table.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+void starline();
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    starline();
+    cout << "Тип данных Диапазон" << endl;
+    starline();
+    cout << "char -128...127" << endl
+    << "short -32 768...32 767" << endl
+    << "int Системно-зависимый" << endl
+    << "long -2 147 483 648...2 147 483 647" << endl;
+    starline();
+
+   return 0;
+}
+
+void starline()
+{
+    for(int i = 0; i < 45; i++) {
+        cout << '*';
+    }
+    cout << endl;
+}
+
+
+
