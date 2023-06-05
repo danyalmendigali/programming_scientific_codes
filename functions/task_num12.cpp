2ce7605db571e05d3f5db842ec9dbc8e6de45718
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-05 22:04:51 +0600

    functions

---
+++ functions/task_num12.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// П
+
+int sumOfArraytVowels(int* arr, int n);
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int n;
+    cout << "Введите количество чисел в масcиве: ";
+    cin >> n;
+    int arr[n];
+    cout << "Заполнте массив" << endl;
+    FOR(i, 0, n) {
+        cout << "Элемен #" << i + 1 << ": ";
+        cin >> arr[i];
+    }
+    cout << "Cумма элементов массива: " <<  sumOfArraytVowels(arr, n);
+
+
+
+
+
+
+  return 0;
+}
+
+int sumOfArraytVowels(int* arr, int n)
+{
+    int sum = 0;
+    FOR(i, 0, n) {
+        sum += arr[i];
+    }
+
+    return sum;
+
+}
