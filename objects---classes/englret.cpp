5075045da2eaeb2f4035f40ac1566637f4599121
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-06 22:49:52 +0600

    objects_classes

---
+++ objects---classes/englret.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h> // для getch()
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+
+// инициализация объектов с помощью копирующего конструктора
+
+class Distance
+{
+private:
+    int feet;
+    int inches;
+public:
+    Distance() : feet(0), inches(0.0) { }
+
+    Distance(int ft, float in) : feet(ft), inches(in) { }
+
+    void getdist() // ввод длины пользователем
+     {
+        cout << "Введите число футов ";
+        cin >> feet;
+        cout << "Введите число дюймов: ";
+        cin >> inches;
+     }
+
+    void showdist() // вывод длины
+     {
+        cout << feet << "\'-" << inches << '\"';
+     }
+
+     Distance add_dist(Distance);
+};
+
+Distance Distance::add_dist(Distance d2)
+{
+     Distance temp;
+     temp.inches = inches + d2.inches;
+     if(temp.inches >= 12.0) {
+        temp.inches -= 12.0;
+        temp.feet = 1;
+     }
+
+
+     return temp;
+}
+
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+     Distance dist1, dist3; // две длины
+     Distance dist2(11, 6.25); // определение и инициализация dist2
+     dist1.getdist(); // ввод dist1 пользователем
+     dist3 = dist1.add_dist(dist2); // dist3 = dist1 + dist2
+
+
+     cout << "dist1 = ";
+     dist1.showdist();
+
+     cout << "\ndist2 = ";
+     dist2.showdist();
+
+     cout << "\ndist3 = ";
+     dist3.showdist();
+
+     cout << endl;
+
+
+
+
+
+  return 0;
+}
