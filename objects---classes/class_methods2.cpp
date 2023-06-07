8625e6a253a7026c6a9f8c225819c49e18e90c49
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-08 00:13:44 +0600

    objects_classes

---
+++ objects---classes/class_methods2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h> // для getch()
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+class Employee
+{
+public:
+    string m_name;
+    int m_id;
+    double m_wage;
+
+    void print() // Метод вывода информации о работнике на экран
+    {
+        cout << "Name: " << m_name << "\nId: " << m_id << "\nWage: $" << m_wage << "\n";
+    }
+};
+
+
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    // Определяем двух работников
+    Employee john { "John", 5, 30.00 };
+    Employee maxim { "Maxim", 6, 32.75 };
+
+
+    // Выводим информацию о работниках на экран
+    john.print();
+    cout << endl;
+
+    maxim.print();
+    cout << endl;
+
+
+
+
+
+  return 0;
+}
+
+
