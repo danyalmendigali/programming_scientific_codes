619d4d0f72cc7f8d5823894c578dffb83d2c60f8
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-08 22:32:02 +0600

    objects_classes

---
+++ objects---classes/constructor.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Конструктор класса с параметрами. Конструктор по умолчанию
+
+class Human
+{
+public:
+    int age;
+    int weight;
+    string name;
+
+};
+
+class Point
+{
+private:
+    int x;
+    int y;
+
+public:
+
+    Point(int valueX, int valueY) // Конструктор с параметрами(Должен быть всегда в public)
+    {
+        x = valueX;
+        y = valueY;
+    }
+
+    int GetY() // Геттер
+    {
+        return y;
+    }
+
+    void SetY(int valueY) // Сеттер
+    {
+        y = valueY; // Умножил значение на два
+    }
+
+    int GetX() // Геттер
+    {
+        return x;
+    }
+
+    void SetX(int valueX) // Сеттер
+    {
+        x = valueX;
+    }
+
+
+
+    void Print()
+    {
+        cout << "x = " << x << "\ny = " << y << endl << endl;
+    }
+
+};
+
+class CoffeeGrinder
+{
+private:
+    bool CheckVoltage() // Если вольтаж в норме
+    {
+        return false; // Выводит неправду
+    }
+
+public:
+    void Start()
+    {
+        bool voltageIsNormal = CheckVoltage(); // Вольтаж в норме
+
+        if(voltageIsNormal) {  // Если вольтаж в норме выводит 'VjuHHH!'
+        cout << "VjuHHH!" << endl;
+        }
+
+        else {
+            cout << "Beep Beep" << endl; // Иначе 'Beep Beep'
+        }
+    }
+
+
+};
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    Point a(5, 11); // передали параметры для конструктора с параметрами
+    a.Print();
+
+
+
+
+   return 0;
+}
+
+
+
