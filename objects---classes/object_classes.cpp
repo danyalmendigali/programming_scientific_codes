1280e22994913d01b23dbba9b9674921a6273ffa
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-08 19:44:46 +0600

    objects_classes

---
+++ objects---classes/object_classes.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+class Human
+{
+public:
+    int age;
+    int weight;
+    string name;
+
+};
+
+class Point
+{
+public:
+    int x;
+    int y;
+    int z;
+};
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+/*
+    Human firstHuman;
+    firstHuman.age = 30;
+    firstHuman.name = "Ivan Ivanovich";
+    firstHuman.weight = 100;
+
+    cout << firstHuman.age << endl;
+    cout << firstHuman.name << endl;
+    cout << firstHuman.weight << endl;
+    cout << endl;
+
+    cout << "------------------------" << endl << endl;
+
+    Human secondHuman;
+    secondHuman.age = 12;
+    secondHuman.name = "Maksim Maksimovich";
+    secondHuman.weight = 80;
+
+    cout << secondHuman.age << endl;
+    cout << secondHuman.name << endl;
+    cout << secondHuman.weight << endl;
+*/
+
+
+    Point a;
+    a.x = 3;
+    a.y = 2;
+    a.z = 1;
+
+
+
+
+
+   return 0;
+}
