29a7a1feed439532eedd7c9c528c77721dff5212
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 23:29:43 +0600

    objects_classes

---
+++ objects---classes/interfaces_with_abstract_classes.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Интерфейсы с помощью абстрактных классов
+
+class MyInterface {
+public:
+    virtual void myMethod() = 0;
+};
+
+class MyClass : public MyInterface {
+public:
+    void myMethod() override {
+        // Реализация метода интерфейса
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
