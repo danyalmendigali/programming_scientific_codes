11ad67794be9d90f4865321fab7b415252ef7eed
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 22:50:57 +0600

    objects_classes

---
+++ objects---classes/methods_attributes.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Пример класса с методами и атрибутами
+
+class MyClass {
+private:
+    int myAttribute;
+
+public:
+    MyClass() {
+        // Конструктор класса
+        myAttribute = 0;
+    }
+
+    void setAttribute(int value) {
+        // Метод для установки значения атрибута
+        myAttribute = value;
+    }
+
+    int getAttribute() {
+        // Метод для получения значения атрибута
+        return myAttribute;
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
