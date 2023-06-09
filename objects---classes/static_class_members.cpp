d27f44afb88d957fa4c2c9aedb9e32a5d42b9995
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 22:58:45 +0600

    objects_classes

---
+++ objects---classes/static_class_members.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Статические члены класса
+
+class MyClass {
+public:
+    static int myStaticAttribute;
+
+    static void myStaticMethod() {
+        // Статический метод класса
+    }
+};
+
+// Инициализация статического атрибута вне класса
+int MyClass::myStaticAttribute = 0;
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
+
