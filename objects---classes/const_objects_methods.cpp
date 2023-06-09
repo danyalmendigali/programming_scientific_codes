1e9eca8ea661b9c61d927d8eb1d7e0b69994e93e
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 23:25:39 +0600

    objects_classes

---
+++ objects---classes/const_objects_methods.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Константные объекты и методы
+
+class MyClass {
+public:
+    void myMethod() {
+        // Метод, который не изменяет состояние объекта
+    }
+};
+
+int main() {
+    const MyClass obj; // Константный объект
+    obj.myMethod(); // Вызов константного метода
+    return 0;
+}
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
