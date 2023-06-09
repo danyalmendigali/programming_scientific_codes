465c89381970879ab4dbd625213569930ee6874c
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 23:27:04 +0600

    objects_classes

---
+++ objects---classes/const_class_members.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Константные члены класса
+
+class MyClass {
+private:
+    const int myConstant = 10; // Константный член класса
+
+public:
+    void myMethod() {
+        // Использование константного члена класса
+        int result = myConstant * 2;
+        // ...
+    }
+}
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
