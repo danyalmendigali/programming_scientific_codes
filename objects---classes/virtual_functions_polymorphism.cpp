26f1240e1c3a05cd71130c3619d6cb4edd81ca9d
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 22:54:04 +0600

    objects_classes

---
+++ objects---classes/virtual_functions_polymorphism.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Виртуальные функции и полиморфизм
+
+class MyBaseClass {
+public:
+    virtual void myMethod() {
+        // Виртуальная функция базового класса
+    }
+};
+
+class MyDerivedClass : public MyBaseClass {
+public:
+    void myMethod() override {
+        // Переопределение виртуальной функции в производном классе
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
