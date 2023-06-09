18a8740d572f3e71db470cbcd052180f91b91259
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 22:55:47 +0600

    objects_classes

---
+++ objects---classes/constructors_destructors.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Конструкторы и деструкторы
+
+class MyClass {
+public:
+    MyClass() {
+        // Конструктор по умолчанию
+    }
+
+    MyClass(int value) {
+        // Конструктор с параметром
+    }
+
+    ~MyClass() {
+        // Деструктор
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
