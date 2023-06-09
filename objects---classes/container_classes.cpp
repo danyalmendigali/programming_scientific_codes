b8a7784c525efe20c581b1fb12a56d825dc33685
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 23:30:57 +0600

    objects_classes

---
+++ objects---classes/container_classes.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Классы-контейнеры
+
+class MyContainer {
+private:
+    std::vector<int> data;
+
+public:
+    void addValue(int value) {
+        data.push_back(value);
+    }
+
+    int getValueAtIndex(int index) {
+        return data[index];
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
