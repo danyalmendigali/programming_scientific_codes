a0ef5c8c13942732467558416598dea4f441bf29
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 23:18:25 +0600

    objects_classes

---
+++ objects---classes/constant_methods.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Константные методы
+
+class MyClass {
+private:
+    int myAttribute;
+
+public:
+    int getAttribute() const {
+        // Константный метод для получения значения атрибута
+        return myAttribute;
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
+
