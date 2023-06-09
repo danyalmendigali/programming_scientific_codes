934c9a642a171682da2ebe60f225c664821079ed
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 22:57:28 +0600

    objects_classes

---
+++ objects---classes/encapsulation_public_methods.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Инкапсуляция и доступ к закрытым атрибутам через открытые методы:
+
+class MyClass {
+private:
+    int myAttribute;
+
+public:
+    void setAttribute(int value) {
+        // Метод для установки значения атрибута
+        myAttribute = value;
+    }
+
+    int getAttribute() {
+        // Метод для получения значения атрибута
+        return myAttribute;
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
+
+
