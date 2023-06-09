6103f527dbab065fb1aef7e1ca10282faa11ea0e
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-09 22:52:43 +0600

    objects_classes

---
+++ objects---classes/class_Inheritance.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Наследование классов
+
+class MyBaseClass {
+public:
+    void baseMethod() {
+        // Метод базового класса
+    }
+};
+
+class MyDerivedClass : public MyBaseClass {
+public:
+    void derivedMethod() {
+        // Метод производного класса
+    }
+};
+
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+
+
+
+    return 0;
+}
+
