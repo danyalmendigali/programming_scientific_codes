cb2f1b9de4ff8bf65e6a1e2b85dcac87cba54b26
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-11 02:17:54 +0600

    string_char

---
+++ string_char_scientific_codes/string_size()_function.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a, i < b; i++)
+
+// length() или size(): Возвращает длину строки.
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string s;
+    cout <<  "Введите строку: ";
+    cin >> s;
+    int length = s.size(); // или int length = s.length();
+    cout << "Длинна строки: " << length;
+
+
+
+
+
+
+   return 0;
+}
+
