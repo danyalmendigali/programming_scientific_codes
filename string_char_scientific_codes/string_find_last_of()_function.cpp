4292466cbd7f040dc2487864a6ca266fba03b576
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-11 22:58:26 +0600

    stirng_char

---
+++ string_char_scientific_codes/string_find_last_of()_function.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// find_last_of(): Ищет последнее вхождение любого символа из указанного набора символов и возвращает его позицию.
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string str = "Hello, world!";
+    int pos = str.find_last_of("abcdefghijklmnopqrstuvwxyz"); // Результат: 11 (последнее вхождение буквы 'l')
+
+    cout << pos << endl;
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
+
+
+
+
