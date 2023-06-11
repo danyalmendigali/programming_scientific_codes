7758011534d6cbf03859377ec609b5b4f7c9cf08
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-11 23:16:27 +0600

    string_char

---
+++ string_char_scientific_codes/string_c_str()_function.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// c_str(): Возвращает указатель на нуль-терминированную строку (массив символов) с тем же содержимым, что и у строки.
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string str = "Hello, World!";
+
+    const char* cstr = str.c_str(); // cstr указывает на "Hello, World!\0"
+    cout << cstr;
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
+
+
+
+
+
+
+
+
