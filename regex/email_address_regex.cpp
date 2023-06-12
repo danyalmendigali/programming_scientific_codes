17d86f1c5438cd073881b007f9f02f48fd8835c8
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-12 11:21:50 +0600

    regex

---
+++ regex/email_address_regex.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a, i < b; i++)
+
+// Поиск и извлечение всех email-адресов из текс
+
+int main()
+{
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string text = "Contact us at info@example.com or support@example.com";
+    regex regex(R"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)");
+
+    sregex_iterator iterator(text.begin(), text.end(), regex);
+    sregex_iterator end;
+
+    while(iterator != end) {
+        smatch match = *iterator;
+        cout << "Найден email: " << match.str() << endl;
+        ++iterator;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
