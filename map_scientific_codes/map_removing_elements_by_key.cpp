3f4fab3dd3e5991433ef33fdc9242b235d2c58c7
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-14 22:16:28 +0600

    map

---
+++ map_scientific_codes/map_removing_elements_by_key.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Удаление элементов по ключу из map:
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    map <string, int> scores;
+
+    // Вставка элементов
+    scores["Alice"] = 90;
+    scores["Bob"] = 80;
+    scores["Charlie"] = 95;
+
+    // Удаление элементов по ключу
+    string key = "Bob";
+    scores.erase(key);
+
+    // Вывод результатов
+    for (const auto& pair : scores) {
+        cout << "Name: " << pair.first << ", Score: " << pair.second << endl;
+    }
+
+
+  return 0;
+}
+
+
+
