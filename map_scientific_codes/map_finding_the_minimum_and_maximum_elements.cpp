1a52f5ada2be77a8bf56ecb543bbaa995c5c2eeb
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-14 22:02:49 +0600

    map

---
+++ map_scientific_codes/map_finding_the_minimum_and_maximum_elements.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Поиск минимального и максимального элементов в map:
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    map <string, int> scores;
+
+    // Вставка элементов
+    scores["Alice"] = 90;
+    scores["Bob"] = 80;
+    scores["Charlie"] = 95;
+
+    // Поиск минимального и максимального элементов
+    auto minElement = min_element(scores.begin(), scores.end());
+    auto maxElement = max_element(scores.begin(), scores.end());
+
+    // Вывод результатов
+    cout << "Min Element: " << minElement->first << " - " << minElement->second << endl;
+    cout << "Max Element: " << maxElement->first << " - " << maxElement->second << endl;
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
