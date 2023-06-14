1ef47744580537be2f62f38617551a8d72ac30c1
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-14 20:41:54 +0600

    map

---
+++ map_scientific_codes/map_presence_of_elements.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long longas
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Проверка наличия элемента в map:
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    map <string, int> scores;
+
+    // Вставка элементов
+    scores["Alice"] = 90;
+    scores["Bob"] = 80;
+    scores["Charlie"] = 95;
+
+    // Проверка наличия элемента
+    string name = "Alice";
+    if (scores.count(name) > 0) {
+        cout << name << " is present in the map" << endl;
+    } else {
+        cout << name << " is not present in the map" << endl;
+    }
+
+
+
+
+
+  return 0;
+}
+
+
+
+
