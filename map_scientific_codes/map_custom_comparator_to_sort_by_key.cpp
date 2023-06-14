2a0dc99e6c24a0309669fc8f8e09d1b92b52b4e5
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-14 21:34:33 +0600

    map

---
+++ map_scientific_codes/map_custom_comparator_to_sort_by_key.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Использование map с пользовательским компаратором для сортировки по ключу:
+
+struct ReverseCompare {
+    bool operator()(const string& str1, const string& str2) const {
+        return str1 > str2;
+    }
+};
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    map <string, int, ReverseCompare> scores;
+
+    // Вставка элементов
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
