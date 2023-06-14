3e474f10ebe5c86e02559800aff750a5a91580d5
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-14 20:52:48 +0600

    map

---
+++ map_scientific_codes/map_removing_al_ items.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Удаление всех элементов из map:
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    map <string, int> scores;
+
+    // Вставка элементов
+    scores["Alice"] = 90;
+    scores["Bob"] = 80;
+    scores["Charlie"] = 95;
+
+    // Удаление всех элементов
+    scores.clear();
+    cout << "Number of elements in the map: " << scores.size() << endl;
+
+
+
+  return 0;
+}
+
+
+
+
+
