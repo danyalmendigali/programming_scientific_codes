c71f3f82d969713f5df58a24d96a978f467ab332
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-14 21:25:40 +0600

    map

---
+++ map_scientific_codes/map_Iterate_over_a_range_of_values.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Итерация по диапазону значений в map:
+
+int main() {
+    cin.tie(0);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    map <string, int> scores;
+
+    // Вставка элементов
+    scores["Alice"] = 90;
+    scores["Bob"] = 80;
+    scores["Charlie"] = 95;
+
+    // Итерация по диапазону значений
+    for (const auto& pair : scores) {
+        cout << "Score: " << pair.second << endl;
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
