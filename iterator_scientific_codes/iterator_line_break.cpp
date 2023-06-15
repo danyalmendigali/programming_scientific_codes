0fd69e4a59d92f14305852a7be9716c00474a396
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-15 23:47:06 +0600

    iterator

---
+++ iterator_scientific_codes/iterator_line_break.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Использование итератора для разворота строки:
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string text = "Hello, world!";
+
+    // Разворот строки с использованием итераторов
+    reverse(text.begin(), text.end());
+
+    cout << text << endl;
+
+
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
