33f031b7a31d6c36a7df24cd3b10aff28a79e4a2
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-15 23:21:17 +0600

    iterator

---
+++ iterator_scientific_codes/iterator_using_an_iterator_to_replace_elements.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Использование итератора для замены элементов в контейнере:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    vector <int> numbers = {1, 2, 3, 4, 5};
+
+    // Заменяем все числа 3 на число 10
+    replace(numbers.begin(), numbers.end(), 3, 10);
+
+    // Выводим измененные элементы
+    for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+        cout << *it << " ";
+    }
+
+
+
+   return 0;
+}
+
+
+
+
+
