248b68c9eba22969f96a9b5be4a7ecd2bf632b9f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-16 21:19:49 +0600

    queue

---
+++ queue_scientific_codes/queue_checking_if_the_queue_is_empty.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Проверка, является ли очередь пустой:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    queue <int> myQueue;
+
+    // Проверка, является ли очередь пустой
+    if (myQueue.empty()) {
+        cout << "Очередь пуста." << endl;
+    } else {
+        cout << "Очередь не пуста." << endl;
+    }
+
+
+   return 0;
+}
+
+
+
