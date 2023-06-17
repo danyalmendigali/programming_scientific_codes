08def6213e50adcc7066f645fe996f47bcc12aad
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-17 13:16:11 +0600

    stack

---
+++ stack_scientific_codes/stack_adding_elements_to_the_stack_and_popping_them.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Добавление элементов в стек и их извлечение:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+    stack <int>  myStack;
+
+
+    myStack.push(10);
+    myStack.push(20);
+    myStack.push(30);
+
+
+    while(!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+
+
+
+
+   return 0;
+}
