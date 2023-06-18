11cafc62afc84c39a0c132553efc4f041ee4dd2a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-18 21:54:02 +0600

    pointers

---
+++ pointers_scientific_codes/ptracc.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <functional>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Доступ к переменной через указатель
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int var1 = 11;
+    int var2 = 22;
+
+    int* ptr;
+
+    ptr = &ptr1;
+    cout << *ptr << endl;
+
+    ptr = &var2;
+    cout << *ptr << endl;
+
+
+
+
+
+   return 0;
+}
+
+z
