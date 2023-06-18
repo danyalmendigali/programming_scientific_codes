60973b7e609d1bd528a8c4e5c223dd5094864b98
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-18 21:58:29 +0600

    string

---
+++ string_char_scientific_codes/string_concatenation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Соединение строк:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string str1 = "Hello,";
+    string str2 = " world!";
+    string result = str1 + str2; // Соединение строк
+
+    cout << result << endl;
+
+
+
+
+   return 0;
+}
+
