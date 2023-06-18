161121a7c10b2573de6dc6cb8708a8d8002d6b5f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-18 15:21:27 +0600

    pointers

---
+++ pointers_scientific_codes/pointers_program_for_working_with_pointers_to_functions_with _rguments.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Программа для работы с указателями на функции с аргументами:
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int (*ptr)(int, int); // Указатель на функцию с аргументами и возвращаемым значением
+
+
+    ptr = add; // Присваивание указателю адреса функции add
+    int result = ptr(5, 3); // Вызов функции через указатель
+    cout << "Результат сложения: " << result << endl;
+
+
+    ptr = subtract; // Присваивание указателю адреса функции subtract
+    result = ptr(5, 3); // Вызов функции через указатель
+    cout << "Результат вычитания: " << result << endl;
+
+
+   return 0;
+}
+
