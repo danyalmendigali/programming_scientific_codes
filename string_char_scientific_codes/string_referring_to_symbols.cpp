28ca62562b45e08990e2c49eb1bf1e78edd8100d
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-18 21:56:28 +0600

    string

---
+++ string_char_scientific_codes/string_referring_to_symbols.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <functional>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Обращение к символам строки:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string str = "Hello, world!";
+
+    char firstChar = str[0]; // Обращение к символу по индексу
+    char lastChar = str[str.length() - 1];
+
+    cout << "First character: " << firstChar << endl;
+    cout << "Last character: " << lastChar << endl;
+
+
+
+
+
+   return 0;
+}
