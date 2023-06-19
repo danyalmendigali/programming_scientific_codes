e8523be26579c4d0fcc5a0cbecd9c89ffee36573
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-19 15:20:56 +0600

    new_delete

---
+++ new_delete_scientific_codes/new_delete_creating_and_deleting_an_array_of_objects.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Создание и удаление массива объектов:
+
+class MyClass {
+public:
+    MyClass() {
+        cout << "Конструктор" << endl;
+    }
+
+    ~MyClass() {
+        cout << "Деструктор" << endl;
+    }
+};
+
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int size = 5;
+    MyClass* arr = new MyClass[size]; // Создание массива объектов
+
+    // Использование массива объектов
+
+    delete[] arr; // Удаление массива объектов
+
+
+
+
+   return 0;
+}
+
+
