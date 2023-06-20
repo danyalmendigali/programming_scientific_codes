49ea0246d9919ffe0e489b9b04b34dbcc8c39768
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-20 23:36:47 +0600

    set

---
+++ set_scientific_codes/set_check_for_emptiness_set.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <sstream>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Проверка на пустоту set:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    set <int> set;
+
+    if (set.empty()) {
+        cout << "Сет пустой" << endl;
+    } else {
+        cout << "Сет не пустой" << endl;
+    }
+
+
+   return 0;
+}
+
+
+
+
+
