f425e09bfdb2db8bd06656799ed0a4b291eeef28
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-20 20:12:36 +0600

    deque

---
+++ deque_scientific_codes/deque_removing_the_last_element.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Удаление последнего элемента из deque:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    deque <int> deque = {1, 2, 3, 4, 5};
+
+    deque.pop_back(); // Удаление последнего элемента
+
+    for (int i : deque) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+
+
+   return 0;
+}
+
