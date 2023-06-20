a4986857fe65104f4ac9061fd31f46dd94124e69
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-20 19:23:25 +0600

    deque

---
+++ deque_scientific_codes/deque_iterating_over_deque_using_range_loop.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Итерирование по deque с использованием диапазонного цикла (C++11):
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+    deque <int> deque1 = {1, 2, 3, 4, 5};
+
+    deque <int> subDeque(deque1.begin() + 1, deque1.end() - 1);
+
+    for (int i : subDeque) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+
+
+
+   return 0;
+}
+
+
