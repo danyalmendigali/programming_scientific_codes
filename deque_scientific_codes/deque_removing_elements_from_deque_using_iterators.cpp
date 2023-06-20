691f3346180f3e267b19ce2136d9e3dfc4e1cac9
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-20 19:39:29 +0600

    deque

---
+++ deque_scientific_codes/deque_removing_elements_from_deque_using_iterators.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Удаление элементов из deque с использованием итераторов:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    deque<int> deque1 = {1, 2, 3, 4, 5};
+
+    deque <int>::iterator it = deque1.begin() + 1;
+    deque1.erase(it); // Удаление элемента на позиции 1
+
+    for (int i : deque1) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+
+
+
+   return 0;
+}
+
+
