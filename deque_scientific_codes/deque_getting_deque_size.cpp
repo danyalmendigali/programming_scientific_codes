7d6c281be38e3b6dfac47c2ff7616666d27b0e15
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-20 19:27:20 +0600

    deque

---
+++ deque_scientific_codes/deque_getting_deque_size.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Получение размера deque:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    deque<int> deque1 = {1, 2, 3, 4, 5};
+
+    cout << "Размер deque: " << deque1.size() << endl;
+
+
+
+   return 0;
+}
+
+
+
+
