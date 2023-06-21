50d8f2333184a006275c3beb7ddd4f54df7bc676
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-21 22:23:51 +0600

    priority_queue

---
+++ priority_queue_scientific_codes/priority_queue_finding_the_k_smallest_elements_in_an_array.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <sstream>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Поиск пересечения двух массивов с использованием unordered_set:
+
+vector <int> findKSmallestElements(const vector<int>& nums, int k) {
+    priority_queue<int> pq;
+
+    for (int num : nums) {
+        pq.push(num);
+        if (pq.size() > k) {
+            pq.pop();
+        }
+    }
+
+    vector<int> kSmallest;
+    while (!pq.empty()) {
+        kSmallest.push_back(pq.top());
+        pq.pop();
+    }
+
+    return kSmallest;
+}
+
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    vector<int> nums = {9, 4, 7, 2, 5};
+    int k = 3;
+
+    vector<int> kSmallest = findKSmallestElements(nums, k);
+
+    cout << "K наименьших элементов: ";
+    for (int num : kSmallest) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+
+   return 0;
+}
+
