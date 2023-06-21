4cfc78cf2d4ec28c05ca08bac802df081bcbb9fe
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-21 16:07:17 +0600

    unordered

--- unordered_set_scientific_codes/undordered_set_remove_duplicates_from_vector.cpp
+++ unordered_set_scientific_codes/undordered_set_remove_duplicates_from_vector.cpp
@@ -1,1 +1,45 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Удаление дубликатов из вектора с использованием std::unordered_set:
+void removeDuplicates(vector<int>& nums) {
+    unordered_set <int> numSet;
+    vector <int> result;
+
+    for (int num : nums) {
+        if (numSet.insert(num).second) {
+            result.push_back(num);
+        }
+    }
+
+    nums = result;
+}
+
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    vector <int> nums = {1, 2, 3, 4, 5, 2, 4, 6, 7};
+
+    removeDuplicates(nums);
+
+    cout << "Уникальыне элементы: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+
+
+   return 0;
+}
 
