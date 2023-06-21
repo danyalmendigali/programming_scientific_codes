aa8c6a6fc10637423ae695159bed1eb51b6e32eb
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-21 16:08:53 +0600

    unordered_set

--- unordered_set_scientific_codes/unordered_set_counting_the_number_of_unique_elements_in_an_array.cpp
+++ unordered_set_scientific_codes/unordered_set_counting_the_number_of_unique_elements_in_an_array.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Подсчет количества уникальных элементов в массиве с использованием unordered_set:
+
+int countUniqueElements(const vector <int>& nums) {
+    unordered_set <int> numSet(nums.begin(), nums.end());
+    return numSet.size();
+}
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    vector <int> nums = {1, 2, 3, 4, 5, 2, 4, 6, 7};
+
+    int uniqueCount = countUniqueElements(nums);
+
+    cout << "Количество уникальных элементов: " << uniqueCount << endl;
+
+
+
+   return 0;
+}
+
+
+
 
