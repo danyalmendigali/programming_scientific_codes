c387f8d331742e6afba122594ba77df057733828
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-21 12:04:18 +0600

    unordered_map

---
+++ unordered_map_scientific_codes/unordered_map_checking_if_an_element_exists.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Проверка наличия элемента в unordered_map:
+
+
+bool containsElement(const unordered_map <int, string>& map, int key) {
+    return map.find(key) != map.end();
+}
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    unordered_map <int, string> map;
+    map[1] = "apple";
+    map[2] = "orange";
+    map[3] = "banana";
+
+    int key = 2;
+
+    if(containsElement(map, key)) {
+        cout << "Map содержит ключ " << key << endl;
+    }
+    else {
+        cout << "Map не содержит ключ" << key << endl;
+    }
+
+
+   return 0;
+}
+
