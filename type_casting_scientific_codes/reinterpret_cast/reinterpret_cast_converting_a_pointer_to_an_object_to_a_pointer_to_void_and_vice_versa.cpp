43b2f0f0b039e05badb785c80235297167137f6e
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 20:16:19 +0600

    reinterpret_cast

---
+++ type_casting_scientific_codes/reinterpret_cast/reinterpret_cast_converting_a_pointer_to_an_object_to_a_pointer_to_void_and_vice_versa.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <fstream>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Преобразование указателя на объект в указатель на void и обратно
+
+class MyClass {
+public:
+    void print() const {
+        cout << "Это объект класса MyClass." << endl;
+    }
+};
+
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    MyClass myObj;
+    void* voidPtr = reinterpret_cast<void*>(&myObj);
+
+    MyClass* myObjPtr = reinterpret_cast<MyClass*>(voidPtr);
+    myObjPtr->print();
+
+
+   return 0;
+}
+
+
