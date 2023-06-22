12d2d6ccb510df6c45c05076fec9e19daefd577c
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 17:06:38 +0600

    dynamic_cast

---
+++ type_casting_scientific_codes/dynamic_cast/dynamic_cast_converting_a_pointer_to_a_base_class_into_a_pointer_to_a_derived_class_without_type_checking.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <chrono>
+#include <ctime>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Пример использования dynamic_cast для преобразования указателя на базовый класс в указатель на производный класс без проверки типа:
+
+class Base {
+public:
+    virtual void print() {
+        cout << "Базовый класс" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void print() override {
+        cout << "Производный класс" << endl;
+    }
+};
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+
+    Base* basePtr = new Derived();
+    Derived* derivedPtr = dynamic_cast<Derived*>(basePtr);
+
+    derivedPtr->print();
+
+    delete basePtr;
+
+
+
+   return 0;
+}
+
+
