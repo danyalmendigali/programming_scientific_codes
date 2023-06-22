bac64d0515cd01d60373669a5f0070e23a4c6db1
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 20:40:10 +0600

    const_cast

---
+++ type_casting_scientific_codes/const_cast/const_cast_changing_the_value_of_an_object_member_through_a_pointer.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <fstream>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Изменение значения члена объекта через указатель с помощью const_cast
+
+class MyClass {
+public:
+    int value;
+};
+
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    const MyClass obj = {42};
+
+    // Изменение значения члена объекта через указатель с помощью const_cast
+    const MyClass* constPtr = &obj;
+    MyClass* mutablePtr = const_cast<MyClass*>(constPtr);
+    mutablePtr->value = 20;
+
+    cout << "Измененное значение члена объекта: " << obj.value << endl;
+
+   return 0;
+}
+
