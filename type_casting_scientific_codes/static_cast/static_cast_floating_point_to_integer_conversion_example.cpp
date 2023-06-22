73afd50805e030eecd035b59e23ad9d44648e7c7
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 16:41:21 +0600

    static_cast

---
+++ type_casting_scientific_codes/static_cast/static_cast_floating_point_to_integer_conversion_example.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <chrono>
+#include <ctime>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Пример преобразования числа с плавающей запятой в целое число:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    double doubleValue = 10.5;
+    int intValue = static_cast<int>(doubleValue);
+
+    cout << "Преобразование числа с плавающей запятой в целое число: " << intValue << endl;
+
+
+
+   return 0;
+}
+
