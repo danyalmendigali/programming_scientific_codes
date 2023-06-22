a4cea992d6a310a88fd1b3d469fafd720b0a6c20
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 23:22:46 +0600

    auto

---
+++ auto_scientific_codes/auto_program_to_concatenate_two_strings.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <fstream>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Программа для объединения двух строк:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string str1 = "Hello";
+    string str2 = "World";
+
+    // Использование auto для определения типа переменной result
+    auto result = str1 + " " + str2;
+
+    cout << "Результат объединения: " << result << endl;
+
+
+   return 0;
+}
+
+
+
+
