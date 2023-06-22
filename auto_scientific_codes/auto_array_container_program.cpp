139bccee31d48291c2b8c89487b63daa94bb01e0
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 23:54:26 +0600

    auto

---
+++ auto_scientific_codes/auto_array_container_program.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <fstream>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Программа для работы с контейнером array:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    array<int, 5> numbers = {1, 2, 3, 4, 5};
+
+    // Использование auto для определения типа элементов массива
+    for (const auto& number : numbers) {
+        cout << number << " ";
+    }
+
+    cout << endl;
+
+
+   return 0;
+}
+
+
+
+
+
