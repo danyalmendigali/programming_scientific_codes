c63bef68baa961fe202dd5befe77876f7ddf2c15
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-22 16:38:34 +0600

    type_casting

---
+++ DateTime_scientific_codes/date_time_example_of_converting_an_integer_to_a_floating_point_number.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <chrono>
+#include <ctime>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Использование chrono::steady_clock для измерения времени выполнения:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+
+    // Код, время выполнения которого нужно измерить
+
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    chrono::duration<double, milli> elapsed = end - start;
+
+    cout << "Время выполнения: " << elapsed.count() << " мс" << endl;
+
+
+
+   return 0;
+}
+
---
+++ type_casting_scientific_codes/static_cast/static_cast_example_of_converting_an_integer_to_a_floating_point_number.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <iterator>
+#include <chrono>
+#include <ctime>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+
+// Пример преобразования целого числа в число с плавающей запятой:
+
+int main() {
+    cin.tie();
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int intValue = 10;
+    double doubleValue = static_cast<double>(intValue);
+
+    cout << "Преобразование целого числа в число с плавающей запятой: " << doubleValue << endl;
+
+
+
+   return 0;
+}
+
