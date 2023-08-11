7441d114f7bbf69fe9d160731be9550191c77c5b
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-08-12 00:55:44 +0600

    BFS

--- BFS/BFS5 (2).cpp
+++ BFS/BFS5 (2).cpp
@@ -1,126 +1,106 @@
-#include <bits/stdc++.h>
-#include <windows.h>
-#include <iterator>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <queue>
+#include <algorithm>
+#include <windows.h> // для работы с кодировкой консоли в Windows
 using namespace std;
 
-#define ar array
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i < b; i++)
-#define VectorVector vector<vector<int>>
+const int INF = 1e9;  // Большое число для представления бесконечности
 
-// Обход графа в ширину(список ребер) и нахождение кратчайшего пути
-
-
-// edges: список ребер в графе
-// startPoint: стартовая вершина для поиска пути
-// endPoint: целевая вершина, до которой нужно найти кратчайший путь
-
-vector<int> bfs(vector<pair<int, int>> &edges, int startPoint, int endPoint)
+// Функция конвертации списка рёбер в матрицу смежности
+vector<vector<int>> convert(vector<pair<int, int>> &edges, int size_matrix)
 {
-    int n = 0; // n представляет количество вершин в графе
-
-    // Определяем количество вершин в графе, находя максимальный номер вершины в списках ребер
-    for(auto edge : edges)
+    // Инициализация матрицы нулями
+    vector<vector<int>> matrix(size_matrix, vector<int>(size_matrix, 0));
+    for(const auto &edge : edges)
     {
-        n = max(n, max(edge.first, edge.second) + 1);
+        int u = edge.first;   // начало ребра
+        int v = edge.second;  // конец ребра
+        // Заполняем соответствующие ячейки матрицы
+        matrix[u][v] = 1;
+        matrix[v][u] = 1;
     }
+    return matrix;
+}
 
+// Обход графа в ширину и нахождение кратчайшего пути
+void bfs(vector<vector<int>> &matrix, int startPoint, int endPoint)
+{
+    int size_matrix = matrix.size();
+    vector<bool> visited(size_matrix, false);  // Вектор для отметки посещенных вершин
+    vector<int> pred(size_matrix, -1);  // Вектор предшествующих вершин для восстановления пути
 
-    vector <bool> visited(n, false); // Создаем вектор visited, который будет отслеживать, была ли вершина посещена во время обхода
-    vector <int> previous(n, -1); // Создаем вектор previous, который будет хранить информацию о предыдущей вершине в кратчайшем пути
-    queue <int> q; // Создаем очередь q, которая будет использоваться для хранения вершин, которые нужно посетить
-
-    q.push(startPoint); // Начальная вершина startPoint помещается в конец очереди q, чтобы начать обход с этой вершины
-    visited[startPoint] = true; // Флаг посещения для начальной вершины startPoint устанавливается в true, так как мы её уже посетили и она находится в очереди
-
-    while(!q.empty()) // Цикл будет выполняться, пока очередь q не станет пустой
+    queue<int> q;  // Очередь для BFS
+    q.push(startPoint);
+    visited[startPoint] = true;
+    while(!q.empty())
     {
-        int numFront = q.front(); // Извлекаем вершину из начала очереди q и сохраним ее в переменную numFront
-        q.pop(); // Удаляем вершину из начала очереди q, так как мы ее уже посетили и обработали
-
-        cout << "Посещена вершина: " << numFront << "\n"; // Выводим сообщение о том, что вершина numFront была посещена
-
-        for(auto edge : edges) // Запускаем цикл для перебора всех ребер, чтобы найти смежные вершины с вершиной numFront
+        int numFront = q.front();
+        q.pop();
+        cout << "Посещена вершина: " << numFront << "\n";
+        for(int i = 0; i < size_matrix; i++)
         {
-            int u = edge.first;
-            int v = edge.second;
-
-            if(u == numFront && !visited[v]) // Проверяем, является ли вершина v смежной с вершиной numFront
+            // Если вершина смежная и не посещена, добавляем в очередь
+            if(matrix[numFront][i] != 0 && !visited[i])
             {
-                q.push(v); // Если вершина v не была посещена, то добавляем ее в конец очереди q, чтобы посетить ее позже
-                visited[v] = true; // Устанавливается флаг посещения для вершины v в значение true, так как мы ее добавили в очередь и посетили позже
-                previous[v] = numFront; // Записываем информацию о том, что вершина v была достигнута из вершины numFront
-            }
-
-            if(v = numFront && !visited[u]) // Проверяем, является ли вершиной u смежной с вершиной numFront
-            {
-                q.push(u); // Если вершина u не была посещена, то добавляем ее в конец очереди q, чтобы посетить ее позже
-                visited[u] = true; // Устанавливается флаг посещения для вершины u в значение true, так как мы ее добавили в очередь и посетили позже
-                previous[u] = numFront; // Записываем информацию о том, что вершина u была достигнута из вершины numFront
+                q.push(i);
+                visited[i] = true;
+                pred[i] = numFront;  // Сохраняем предшествующую вершину
             }
         }
     }
 
-    // Восстановление кратчайшего пути от стартовой вершины к целевой
-    vector <int> shortestPath;
-    int currentVertex = endPoint;
-    while(currentVertex != -1) // Пока не достгнута стартовая вершина(значение -1)
+    // Восстановление кратчайшего пути из конечной вершины в начальную
+    vector<int> shortestWay;
+    cout << "Кратчайший путь: ";
+    for(int i = endPoint; i != -1; i = pred[i])
     {
-        shortestPath.push_back(currentVertex); // Добавляем текущую вершину в кратчайший путь
-        currentVertex = previous[currentVertex]; // Переходим к предыдущей вершине в кратчайшем пути
+        shortestWay.push_back(i);
     }
-
-    reverse(shortestPath.begin(), shortestPath.end()); // Обратный порядок вершин для получения пути от стартовой к целевой
-
-    return shortestPath; // Возвращаем кратчайший путь от стартовой вершины к целевой
-
+    // Вывод пути в правильном порядке
+    for(int i = shortestWay.size() - 1; i >= 0; i--)
+    {
+        cout << shortestWay[i] << " ";
+    }
+    cout << "\n";
 }
 
-int main()
-{
-    cin.tie();
+int main() {
+    cin.tie();  // Отвязываем cin от cout для быстродействия
+    // Настройка кодировки консоли для корректного вывода кириллицы
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int n, m; // Количество вершин и ребер в графе
-    cout << "Введите количество вершин и ребер: ";
-    cin >> n >> m;
+    int n, m; // n - число вершин, m - число рёбер
 
-    vector<pair<int, int>> edges; // Создаем список ребер
+    cout << "Введите количество вершин: ";
+    cin >> n;
 
-    cout << "Введите ребра в форме(u, v): " << "\n";
+    cout << "Введите количество ребер: ";
+    cin >> m;
+
+    vector<pair<int, int>> edges;  // Список рёбер
+
+    cout << "Введите ребра в формате(u, v): " << "\n";
     for(int i = 0; i < m; i++)
     {
-        int u, v;
+        int u, v;  // u - начало ребра, v - конец ребра
         cin >> u >> v;
-        edges.push_back({u, v}); // Добавляем ребро (u, v) в список ребер
+        edges.push_back({u, v});
     }
 
+    // Конвертируем список рёбер в матрицу смежности
+    vector<vector<int>> matrix = convert(edges, n);
 
-    int startPoint, endPoint;
-    cout << "Введите стартовую вершину: ";
+    int startPoint, endPoint;  // Начальная и конечная вершины для поиска пути
+    cout << "Введите начальную вершину: ";
     cin >> startPoint;
     cout << "Введите конечную вершину: ";
     cin >> endPoint;
 
-    vector <int> shortestPath = bfs(edges, startPoint, endPoint); // Вызываем функцию bfs для поиска кратчайшего пути
-
-    if(shortestPath.empty()) // Если кратчайший путь не найден
-    {
-        cout << "Кратчайший путь не найден" << "\n";
-    }
-
-    else // Если кратчайший путь найден
-    {
-        cout << "Кратчайший путь: ";
-        for(int vertex : shortestPath)
-        {
-            cout << vertex << " "; // Выводим вершины кратчайшего пути на экран
-        }
-        cout << "\n";
-    }
-
-
+    // Запуск BFS для поиска кратчайшего пути
+    bfs(matrix, startPoint, endPoint);
 
     return 0;
 }
